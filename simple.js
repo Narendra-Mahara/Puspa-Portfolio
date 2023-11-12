c8c3d55481af0f7be5f9ffea405988757872e9ec
Narendra Mahara <narendramahara85131@gmail.com>
2023-11-12 16:33:04 +0545

    All file uploaded

---
+++ simple.js
@@ -0,0 +1,8 @@
+
+
+    function formsubmit() {
+        // You can add form validation logic here before showing the success message
+        // For simplicity, we'll just show the success message directly
+        alert('Your form submitted successfully');
+        
+    }
